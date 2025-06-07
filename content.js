1094daf08230f0c643f5e1b83fef688d7ee24429
Fiona <fiona2016@protonmail.com>
2025-06-07 00:03:02 -0700

    Add save draft button.

--- content.js
+++ content.js
@@ -2,6 +2,7 @@ function isValidSocialMediaPage() {
   const footer = document.querySelector('.link-footer');
   if (!footer) return false;
 
+  const links = Array.from(footer.querySelectorAll('a[href]'));
   const hasJoin = links.some(link => link.href.includes('https://joinmastodon.org/'));
   const hasGit = links.some(link => link.href.includes('https://github.com/mastodon/mastodon'));
 
@@ -9,14 +10,21 @@ function isValidSocialMediaPage() {
 }
 
 function addDraftButton() {
+    if (!isValidSocialMediaPage()) return;
+    console.log('Adding Draft Button');
     dropdown = document.querySelector('.compose-form__dropdowns');
-
+    if (dropdown && !document.querySelector('.compose-form__button.save-draft')) {
+        saveDraft = document.createElement('button');
+        saveDraft.className = 'compose-form__button.save-draft';
+        saveDraft.textContent = 'Save Draft';
+        dropdown.appendChild(saveDraft);
+    }
 }
 
-const observer = new MutationObserver(() => {
-  addDraftButton();
-});
-observer.observe(document.body, { childList: true, subtree: true });
+// const observer = new MutationObserver(() => {
+//   addDraftButton();
+// });
+// observer.observe(document.body, { childList: true, subtree: true });
 
 // Initial check on load
 addDraftButton();
